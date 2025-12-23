f30745f148c8e21bc9af80cf3f857585580086f7
daim43012 <vgatsko.21@gmail.com>
2025-12-24 01:46:48 +0300

    content route page

---
+++ src/routes/content/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import Content from "../../components/pages/Content.svelte";
+  export let data: PageData;
+</script>
+
+<Content {data} />
