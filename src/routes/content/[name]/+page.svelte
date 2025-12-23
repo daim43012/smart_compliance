fe241a106ef51233b63f9520dc7f8289e2ef8ab2
daim43012 <vgatsko.21@gmail.com>
2025-12-24 01:47:20 +0300

    selected post route

---
+++ src/routes/content/[name]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import Post from "../../../components/pages/Post.svelte";
+
+  export let data: PageData;
+</script>
+
+<Post {data} />
